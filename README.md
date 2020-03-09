e60137f9b7f7b77edecfc6889d58991d720861e6
juliane nagao <julianens@gmail.com>
2020-03-09 19:23:21 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# useReducer-useFetch
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './styles.scss'
+
+const fetchReducer = (state, action) => {
+  switch (action.type) {
+    case 'request':
+      return {
+        ...state,
+        loading: true,
+      }
+    case 'success':
+      return {
+        ...state,
+        data: action.data,
+        error: null,
+        loading: false,
+      }
+
+    case 'failure':
+      return {
+        ...state,
+        error: `Error fetching data. Try again.`,
+        loading: false,
+      }
+    default:
+      throw new Error(`That action type isn't supported.`)
+  }
+}
+
+const initialState = {
+  loading: true,
+  data: null,
+  error: null,
+}
+
+function useFetch(url) {
+  const [state, dispatch] = React.useReducer(fetchReducer, initialState)
+
+  React.useEffect(() => {
+    dispatch({ type: 'request' })
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'success', data }))
+      .catch((e) => {
+        console.warn(e.message)
+        dispatch({ type: 'failure' })
+      })
+  }, [url])
+
+  return {
+    loading: state.loading,
+    data: state.data,
+    error: state.error,
+  }
+}
+
+const postIds = [1, 2, 3, 4, 5, 6, 7, 8]
+
+function App() {
+  const [index, setIndex] = React.useState(0)
+
+  const { loading, data: post, error } = useFetch(
+    `https://jsonplaceholder.typicode.com/posts/${postIds[index]}`,
+  )
+
+  const incrementIndex = () => {
+    setIndex((i) => (i === postIds.length - 1 ? i : i + 1))
+  }
+
+  if (loading === true) {
+    return <p>Loading</p>
+  }
+
+  if (error) {
+    return (
+      <React.Fragment>
+        <p>{error}</p>
+        <button onClick={incrementIndex}>Next Post</button>
+      </React.Fragment>
+    )
+  }
+
+  return (
+    <div className="App">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      {error && <p>{error}</p>}
+      {index === postIds.length - 1 ? (
+        <p>No more posts</p>
+      ) : (
+        <button onClick={incrementIndex}>Next Post</button>
+      )}
+    </div>
+  )
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
